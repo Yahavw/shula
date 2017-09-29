7b6502f18dd826cbab99a8a68fd71f60123a9a89
Nir Badt <nirbadt@gmail.com>
2017-09-29 06:13:22 +0200

    Delete rgbMethods.ino

--- rgbMethods.ino
+++
@@ -1,9 +0,0 @@
-//void colors() {
-//  for (int i = 0; i < LEDS_NUMBER; i++) {
-//    //strip.setPixelColor(i, 0, 255, 0);
-//    ledsStatusArray[i][0] = 254;
-//    ledsStatusArray[i][1] = 0;
-//    ledsStatusArray[i][2] = 0;
-//  }
-//  strip.show();
-//}
